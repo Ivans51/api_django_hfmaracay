64159ba8a05e953f93d02e5e75f3949c7badeb0e
Ivans <ivans1041@gmail.com>
2019-10-24 11:54:25 -0400

    enhancement: soft delete and delete user
    Signed-off-by: Ivans <ivans1041@gmail.com>

--- project/appname/urls.py
+++ project/appname/urls.py
@@ -2,13 +2,15 @@ from django.conf.urls import url
 from django.urls import path, include
 from rest_framework import routers
 
-from project.appname.views import UserViewSet
+from project.appname import views
+from .views import UserViewSet
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('rest-auth/', include('rest_auth.urls')),
+    url(r'^user-delete/(?P<pk>\d+)/$', views.delete_profile, name="delete_profile"),
 ]
 
 urlpatterns += router.urls
--- project/appname/views.py
+++ project/appname/views.py
@@ -1,11 +1,21 @@
 from django.contrib.auth import get_user_model
 # Create your views here.
-from rest_framework import viewsets
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import viewsets, status
 
 from project.appname.serializers import UserSerializer
 
 
+@csrf_exempt
+def delete_profile(request, pk):
+    user = get_user_model().objects.get(pk=pk)
+    user.is_active = False
+    user.save()
+    return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+
+
 class UserViewSet(viewsets.ModelViewSet):
     User = get_user_model()
-    queryset = User.objects.all().order_by('-date_joined')
+    queryset = User.objects.filter(is_active=True).order_by('-date_joined')
     serializer_class = UserSerializer
